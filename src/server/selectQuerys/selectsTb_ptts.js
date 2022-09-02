756fcc0cc0bc5c2c3a2c32ef73a8b5c751855d41
Guilherme <guilherme@zscansoftware.com>
2022-09-01 22:57:48 -0300

    Incluindo comentário

--- src/server/selectQuerys/selectsTb_ptts.js
+++ src/server/selectQuerys/selectsTb_ptts.js
@@ -1,6 +1,8 @@
 const zscan_database  = require('../db.js')
 const unificar = require('../dbUnificar.js')
 
+
+//A função selectPttsUnifica faz um inner Join com tb_cnts e aproveita esse inner join para inserir os dados de contatos do paciente
 async function  selectPttsUnificar(){
     const [pacientesUnificar] = await unificar.query(`select * from  tb_ptts as a inner join tb_cnts as b on a.ptts_code = b.cnts_code where a.ptts_dhcr >= "2022-07-01";`)
     return pacientesUnificar
