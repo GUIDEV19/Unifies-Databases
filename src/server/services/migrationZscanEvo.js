0f2cf833680319a07f73185cd569253329079a8b
Guilherme <guilherme@zscansoftware.com>
2022-09-07 12:46:37 -0300

    retirando as chamadas de migração e adicionando as chamadas de CreateSchema e RestoreDump

--- src/server/services/migrationZscanEvo.js
+++ src/server/services/migrationZscanEvo.js
@@ -1,19 +1,15 @@
 const querys = require('../selectQuerys/index.js');
 const migrations = require('../entidades/index.js');
+const createSchema = require('./CreateSchema.js')
+const restoreDump = require('./shellExecs.js');
 
-/* const migrationPtts = require('./tb_ptts.js');
-const migrationCnts = require('./tb_cnts.js');
-const migrationDocs = require('./tb_docs.js');
-const migrationptts_has_docs = require('./ptts_has_docs.js');
-const migrationTplt = require('./tb_tplt.js');
-const migrationExam = require('./tb_exam.js');
-const migrationDvcs = require('./tb_dvcs.js');
-const migrationEmps = require('./tb_emps.js');
-const migrationRprt = require('./tb_rprt.js');
-const migrationImgs = require('./tb_imgs.js'); */
 
 //função administradora de migração
 async function migraDados(){
+    await createSchema()
+    
+    await restoreDump()
+
     //select para pesquisa de pacientes
     const pacientesUnificar = await querys.querysTbPtts.selectPttsUnificar()
 
