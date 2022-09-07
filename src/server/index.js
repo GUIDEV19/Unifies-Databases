eeada7166bae39894c51082e9444ab00b54292e3
Guilherme <guilherme@zscansoftware.com>
2022-09-07 12:49:25 -0300

    Adicionado Return na resposta da rota e adicionando pacote Shelljs

--- src/server/index.js
+++ src/server/index.js
@@ -35,12 +35,13 @@ app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-app.get('/iniciar', async function(req, res){
+app.get('/iniciar', async function(req, res, next){
     try{
         iniciar()
-        return res.status(200).send({
+        var teste =  res.status(200).send({
                     message: 'Iniciado processo'
                 });
+        return teste
     }catch(erro){
         return res.status(500).send({
             message: 'Erro ao iniciar processo'
