f8774374e3fdb085cd131dbc2c89af8758d3ac3d
Guilherme <guilherme@zscansoftware.com>
2022-09-27 08:15:56 -0300

    Criando arquivo para teste das funções

--- src/server/testes/testeUtilisIkap.js
+++ src/server/testes/testeUtilisIkap.js
@@ -1,6 +1,10 @@
 const utilzIkap = require('../utils/ikapUtils')
+const querys = require('../selectQuerys/querysZscanEvo/index.js')
 
-//teste formatação nome
+
+
+async function teste(){
+    //teste formatação nome
 
 const nome = 'Guilherme bezerra dos santos'
 const format = utilzIkap.formatNome(nome)
@@ -29,20 +33,33 @@ const formatEstadoCivil = utilzIkap.formatEstadoCivil(estado_civil)
 const cpf = '708.094.561.50'
 const formatCPF = utilzIkap.formatDoc(cpf)
 
+const cep = '74580-460'
+const CEP = utilzIkap.formatCEP(cep)
+
 const fone = '(51) 3249.5550 / (51) 9962.0208'
 const formatFone = utilzIkap.formatFone(fone)
 
-
-
-
-
-const obj = {
-    estado_cvil: formatEstadoCivil,
-    sx: formatsx,
-    date_nasc: formatdate,
-    nome: format,
-    CPF: formatCPF,
-    Telefone: formatFone
+    const city = 'PORTO ALEGRE'
+    const idCity = await querys.querysTbcity.selectCytsId(city);
+    const obj = {
+        estado_cvil: formatEstadoCivil,
+        sx: formatsx,
+        date_nasc: formatdate,
+        nome: format.firstName,
+        nomeMeio: format.middleName,
+        nomeere: format.lastName,
+        CPF: formatCPF,
+        Telefone: formatFone,
+        cp: CEP,
+        IDCITY: idCity[0].cits_code
+    }
+    return console.log(obj)
 }
 
-console.log(obj)
+
+
+
+
+
+
+teste()
